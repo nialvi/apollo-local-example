9ac7fa09fc8f2965439a926139af6b03ee8c812c
Nikolaev Viktor <maclauddd@gmail.com>
2021-05-14 00:44:11 +0300

    added todo fragment, read data from cache

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -3,6 +3,7 @@ import VueApollo from "vue-apollo";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import typeDefs from "./graphql/typedefs.gql";
 import DataLoader from "dataloader";
+import TodoFragment from "./graphql/todo.fragment.gql";
 
 const userLoader = new DataLoader((ids) =>
   Promise.all(
@@ -16,25 +17,39 @@ const userLoader = new DataLoader((ids) =>
 
 const resolvers = {
   Query: {
-    async todo(_, vars) {
-      const rawTodoData = await fetch(
+    async todo(_, vars, { cache }) {
+      const existingTodo = cache.readFragment({
+        fragment: TodoFragment,
+        id: `ClientTodo:${vars.id}`,
+      });
+
+      if (existingTodo) {
+        return existingTodo;
+      }
+
+      const { userId, ...rawTodoData } = await fetch(
         `https://jsonplaceholder.typicode.com/todos/${vars.id}`
       ).then((res) => res.json());
+
       return {
-        ...rawTodoData,
         __typename: "ClientTodo",
+        ...rawTodoData,
+        user: { id: userId },
       };
     },
+
     async todos() {
       const rawTodosData = await fetch(
         "https://jsonplaceholder.typicode.com/todos"
       ).then((res) => res.json());
 
-      return rawTodosData.map((todo) => ({
+      return rawTodosData.map(({ userId, ...todo }) => ({
         __typename: "ClientTodo",
         ...todo,
+        user: { id: userId },
       }));
     },
+
     async users() {
       const rawUserData = await fetch(
         "https://jsonplaceholder.typicode.com/users"
@@ -49,7 +64,7 @@ const resolvers = {
 
   ClientTodo: {
     async user(parent) {
-      const rawUserData = await userLoader.load(parent.userId);
+      const rawUserData = await userLoader.load(parent.user.id);
 
       return {
         ...rawUserData,
