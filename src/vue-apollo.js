a153e0cbadb4f81e1f56acc215989aa33e525f20
Nikolaev Viktor <maclauddd@gmail.com>
2021-05-13 16:47:24 +0300

    created resolver

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -1,13 +1,24 @@
 import Vue from "vue";
 import VueApollo from "vue-apollo";
+import typeDefs from "./graphql/typedefs.gql";
 
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    users() {
+      console.log("resolver called");
+      return fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+        res.json()
+      );
+    },
+  },
+};
 
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
   resolvers,
+  typeDefs,
 });
 
 Vue.use(VueApollo);
