d3e282da3309f586554dfaee9fc59fcf9a8707b0
Nikolaev Viktor <maclauddd@gmail.com>
2021-05-13 18:10:37 +0300

    added todoList and todo queries

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <div v-if="$apollo.queries.users.loading">Loading...</div>
-    <div v-else>{{ users }}</div>
+    <div>
+      <input type="number" v-model="id" />
+      <div v-if="$apollo.queries.todo.loading">Loading 1 todo...</div>
+      <div v-else>{{ todo }}</div>
+    </div>
+
+    <hr />
+
+    <div>
+      <div v-if="$apollo.queries.todos.loading">Loading...</div>
+      <div v-else>{{ todos }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import usersListQuery from "../graphql/usersList.query.gql";
+import todoListQuery from "../graphql/todoList.query.gql";
+import todoQuery from "../graphql/todo.query.gql";
 
 export default {
   name: "HelloWorld",
@@ -14,9 +25,18 @@ export default {
     msg: String,
   },
   apollo: {
-    users: {
-      query: usersListQuery,
+    todos: {
+      query: todoListQuery,
     },
+    todo: {
+      query: todoQuery,
+      variables() {
+        return { id: this.id };
+      },
+    },
+  },
+  data() {
+    return { id: 1 };
   },
 };
 </script>
--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -6,6 +6,25 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const resolvers = {
   Query: {
+    async todo(_, vars) {
+      const rawTodoData = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${vars.id}`
+      ).then((res) => res.json());
+      return {
+        ...rawTodoData,
+        __typename: "ClientTodo",
+      };
+    },
+    async todos() {
+      const rawTodosData = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      ).then((res) => res.json());
+
+      return rawTodosData.map((todo) => ({
+        __typename: "ClientTodo",
+        ...todo,
+      }));
+    },
     async users() {
       const rawUserData = await fetch(
         "https://jsonplaceholder.typicode.com/users"
@@ -17,9 +36,22 @@ const resolvers = {
       }));
     },
   },
+
+  ClientTodo: {
+    async user(parent) {
+      const rawUserData = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${parent.userId}`
+      ).then((res) => res.json());
+
+      return {
+        ...rawUserData,
+        __typename: "ClientUser",
+      };
+    },
+  },
+
   ClientUser: {
     address(parent) {
-      console.log("resolver address called");
       return {
         __typename: "ClientAddress",
         ...parent.address,
